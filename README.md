bafc2368282cbd1a1ec5e7de9ee9c0111dee4e8b
Joe <joseph.wireman@cvtechonline.net>
2023-10-18 10:24:44 -0500

    Added comments and updated the readme
    Comments added to better understand the code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# sign-up-2
-# I made a signup page for school
-# u can use it where ever
+Created by Joseph Wireman
+Date: September 2022
+
+Project:
+    This was a personal project. I had and idea for an animated signup form.
+
+This project currently has an animation bug where if you swap between the forms too fast the text-boxes will overlap. I have no plans on fixing this as I have created a working version in a React/Tailwind website.
--- s&box.js
+++ s&box.js
@@ -1,5 +1,10 @@
+
+//I used the comments to section off code.
+//I indented the code and not the comments to make it easier to locate the sections.
+
 // OPENING
     function input_deck(){
+        // Grabs all the elements needed to be animated
         let title = document.getElementById('title');
         let signupBtn = document.getElementById('signupBtn');
         let username = document.getElementById('sign_username');
@@ -7,6 +12,8 @@
         let con_password = document.getElementById('confirm_password');
         let email = document.getElementById('sign_email');
 
+        // Uses objects to determind how the animation plays out
+        // I chose to put these variables inside the function as it doesnt need to be used anywhere else, and it only plays once.
         let user_ani = {
             'transform': 'translate(0, 0)',
             'opacity': '1'
@@ -24,8 +31,11 @@
             'opacity': '1'
         }
 
+        // The title and the signupBtn only have 1 condition changing at the start, and thus do not need to be in an object
         title.style.opacity = "1"
         signupBtn.style.opacity = "1"
+
+        // Applys the animations to the elements
         Object.assign(username.style, user_ani);
         Object.assign(password.style, pass_ani);
         Object.assign(con_password.style, conPass_ani);
@@ -33,67 +43,75 @@
     }
 
 // SIGN UP CHANGES
+    // When switching to the login, it fires these two functions
+    function disable_sign(){
+        // Grabs the title and the container for the form
+        let sign_contain = document.getElementById('signup_form_container');
+        let title = document.getElementById('title');
 
-function disable_sign(){
-    let sign_contain = document.getElementById('signup_form_container');
-    let title = document.getElementById('title');
+        // Moves the contents to the left and fades it out
+        sign_contain.style.left = '0';
+        sign_contain.style.opacity = '0';
 
-    sign_contain.style.left = '0';
-    sign_contain.style.opacity = '0';
+        // I did not swap out the title element.
+        // I hid the title for a moment and changed the text inside the element.
+        title.style.transitionDelay = '0s';
+        title.style.opacity = '0';
 
-    title.style.transitionDelay = '0s';
-    title.style.opacity = '0';
-}
-function enable_sign(){
-    let sign_contain = document.getElementById('signup_form_container');
-    let title = document.getElementById('title');
+        //The rest is relatively repetitive.
+        //Same functionality just reversed.
+    }
+    function enable_sign(){
+        let sign_contain = document.getElementById('signup_form_container');
+        let title = document.getElementById('title');
 
-    sign_contain.style.left = '50%';
-    sign_contain.style.opacity = '1';
+        sign_contain.style.left = '50%';
+        sign_contain.style.opacity = '1';
 
-    title.style.transitionDelay = '0.8s';
-    title.style.opacity = '1';
+        title.style.transitionDelay = '0.8s';
+        title.style.opacity = '1';
 
-    document.getElementById('switch').innerHTML = "Login"
-    document.getElementById('title').innerHTML = "Create an account"
-}
+        document.getElementById('switch').innerHTML = "Login"
+        document.getElementById('title').innerHTML = "Create an account"
+    }
 
 // LOGIN CHANGES
-function disable_log(){
-    let log_contain = document.getElementById('login_form_container');
-    let title = document.getElementById('title');
+    // When switching to the signup, it fires these two functions
+    function disable_log(){
+        let log_contain = document.getElementById('login_form_container');
+        let title = document.getElementById('title');
 
-    log_contain.style.left = '100%';
-    log_contain.style.opacity = '0';
+        log_contain.style.left = '100%';
+        log_contain.style.opacity = '0';
 
-    title.style.transitionDelay = '0s';
-    title.style.opacity = '0';
-}
-function enable_log(){
-    let log_contain = document.getElementById('login_form_container');
-    let title = document.getElementById('title');
+        title.style.transitionDelay = '0s';
+        title.style.opacity = '0';
+    }
+    function enable_log(){
+        let log_contain = document.getElementById('login_form_container');
+        let title = document.getElementById('title');
 
-    log_contain.style.left = '50%'
-    log_contain.style.opacity = '1'
+        log_contain.style.left = '50%'
+        log_contain.style.opacity = '1'
 
-    title.style.transitionDelay = '0.8s'
-    title.style.opacity = '1';
+        title.style.transitionDelay = '0.8s'
+        title.style.opacity = '1';
 
-    document.getElementById('switch').innerHTML = "Sign Up"
-    document.getElementById('title').innerHTML = "Login to your account"
+        document.getElementById('switch').innerHTML = "Sign Up"
+        document.getElementById('title').innerHTML = "Login to your account"
 
-}
+    }
 
 // SWITCH INTERFACE
-let trigger = 0
-function switch_interface(){
-    if(trigger == 0){
-        disable_sign();
-        setTimeout(enable_log, 600)
-        trigger = 1
-    }else if(trigger == 1){
-        disable_log();
-        setTimeout(enable_sign, 600)
-        trigger = 0
+    let trigger = 0
+    function switch_interface(){
+        if(trigger == 0){
+            disable_sign();
+            setTimeout(enable_log, 600)
+            trigger = 1
+        }else if(trigger == 1){
+            disable_log();
+            setTimeout(enable_sign, 600)
+            trigger = 0
+        }
     }
-}
